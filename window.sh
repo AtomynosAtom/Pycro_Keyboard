e0fc8846d40879afeca1c4a414c4f5abe0e704f5
Atomynos_Atom <atomynosatom@gmail.com>
2020-05-02 16:07:36 +0530

    added InfoExample.

--- window.sh
+++
@@ -1,8 +0,0 @@
-pids=$(xdotool search --class "gvim")
-for pid in $pids; do
-    name=$(xdotool getwindowname $pid)
-    if [[ $name == *"TODO"* ]]; then
-        #Do what you want, $pid is your sought for PID,
-        #matching both class gvim and TODO in title
-    fi
-done
